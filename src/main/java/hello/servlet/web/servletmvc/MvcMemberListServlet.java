ba500ff8959e746864e93219a85cd5ecec109901
superssol <hansole10@gmail.com>
2022-12-05 13:58:37 +0900

    mvc 패턴 만들어보기

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -1,5 +1,8 @@
 package hello.servlet.web.servletmvc;
 
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,12 +10,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "mvcMemberForServlet", urlPatterns = "/servlet-mvc/members/new-form")
-public class MvcMemberForServlet extends HttpServlet {
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String viewPath = "/WEB-INF/views/new-form.jsp";
+
+        List<Member> members = memberRepository.findAll();
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
